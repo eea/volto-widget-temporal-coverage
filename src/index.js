3b94ba407cce0f72baba92b159f79b4dc0ae7ea2
Alin Voinea <contact@avoinea.com>
2022-10-26 13:34:45 +0300

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import { TemporalWidgetEdit, TemporalWidgetView } from './components';
 
 const applyConfig = (config) => {
-  config.widgets.widget = {
-    ...config.widgets.widget,
-    temporal: TemporalWidgetEdit,
-  };
+  config.widgets.widget.temporal = TemporalWidgetEdit;
 
   // volto-widgets-view
   if (config.widgets.views?.widget) {
