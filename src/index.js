d135a470a54012c6d2ac9fc00e14b394116e9cea
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-10-27 21:45:13 +0300

    add temporal coverage facet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { TemporalWidgetEdit, TemporalWidgetView } from './components';
+import TemporalCoverage from '@eeacms/volto-widget-temporal-coverage/components/theme/SearchBlock/TemporalCoverageFacet/TemporalCoverageFacet';
+import TemporalCoverageFacetFilterListEntry from '@eeacms/volto-widget-temporal-coverage/components/theme/SearchBlock/TemporalCoverageFacet/TemporalCoverageFacetFilterListEntry';
 
 const applyConfig = (config) => {
   config.widgets.widget.temporal = TemporalWidgetEdit;
@@ -6,6 +8,22 @@ const applyConfig = (config) => {
   // volto-widgets-view
   if (config.widgets.views?.widget) {
     config.widgets.views.widget.temporal = TemporalWidgetView;
+  }
+
+  //Add Temporal Coverage Facet
+  if (config?.blocks?.blocksConfig?.search) {
+    config.blocks.blocksConfig.search.extensions.facetWidgets.types = [
+      ...config.blocks.blocksConfig.search.extensions.facetWidgets.types,
+      {
+        id: 'time_covereage',
+        title: 'Temporal Coverage',
+        view: TemporalCoverage,
+        isDefault: false,
+        stateToValue: TemporalCoverage.stateToValue,
+        valueToQuery: TemporalCoverage.valueToQuery,
+        filterListComponent: TemporalCoverageFacetFilterListEntry,
+      },
+    ];
   }
 
   return config;
