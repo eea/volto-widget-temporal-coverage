dfa20bde91111292f1a535d0b21769fe18e927ac
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-10-29 23:09:30 +0200

    Add cypress test

--- cypress/e2e/01-block-basics.cy.js
+++ cypress/e2e/01-block-basics.cy.js
@@ -14,17 +14,19 @@ describe('Blocks Tests', () => {
 
     cy.get('[contenteditable=true]').first().type('{enter}');
 
-    // Add block
+    // Add search block
     cy.get('.ui.basic.icon.button.block-add-button').first().click();
-    cy.get('.blocks-chooser .title').contains('Media').click();
-    cy.get('.content.active.media .button.image').contains('Image').click();
-
+    cy.get('.blocks-chooser .title').contains('Common').click();
+    cy.get('.content.active.common .button.search')
+      .contains('Search')
+      .click({ force: true });
+    // add temporal coverage facet
+    cy.get('#blockform-fieldset-facets').click();
+    cy.get('[aria-label="Add Facet"]').click();
+    cy.get('#field-type-2-facets-0').click();
+    cy.get('.react-select__menu').contains('Temporal Coverage').click();
     // Save
     cy.get('#toolbar-save').click();
     cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
-
-    // then the page view should contain our changes
-    cy.contains('My Add-on Page');
-    cy.get('.block.image');
   });
 });
