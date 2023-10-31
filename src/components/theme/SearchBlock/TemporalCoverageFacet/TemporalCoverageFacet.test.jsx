3e10dece7843bf29f331b307032d6c9124edfa40
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-10-31 16:52:46 +0200

    increase code coverage

--- src/components/theme/SearchBlock/TemporalCoverageFacet/TemporalCoverageFacet.test.jsx
+++ src/components/theme/SearchBlock/TemporalCoverageFacet/TemporalCoverageFacet.test.jsx
@@ -9,6 +9,32 @@ import { Ticks } from 'react-compound-slider';
 import TemporalCoverageFacet from './TemporalCoverageFacet';
 const mockStore = configureStore([thunk]);
 
+it('renders TemporalCoverageFacet', () => {
+  const store = mockStore({
+    intl: {
+      locale: 'en',
+      messages: {},
+    },
+    temporal: [
+      { value: '1890', label: '1890' },
+      { value: '1900-1905', label: '1900-1905' },
+      { value: '1915', label: '1915' },
+    ],
+  });
+
+  const component = render(
+    <Provider store={store}>
+      <TemporalCoverageFacet
+        choices={[
+          { value: '1890', label: '1890' },
+          { value: '1900-1905', label: '1900-1905' },
+          { value: '1915', label: '1915' },
+        ]}
+      />
+    </Provider>,
+  );
+  expect(component.container).toMatchSnapshot();
+});
 it('renders TemporalCoverageFacetFilterListEntry', () => {
   const store = mockStore({
     intl: {
